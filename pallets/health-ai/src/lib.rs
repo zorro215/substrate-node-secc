4dbeb50fe433f653c65361b43eb50c0c5ad46710
tangxianjie <xianjie.tang@vonechain.com>
2021-06-19 16:30:30 +0800

    init json convert

--- pallets/health-ai/src/lib.rs
+++ pallets/health-ai/src/lib.rs
@@ -1,8 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://substrate.dev/docs/en/knowledgebase/runtime/frame>
+use codec::{Decode, Encode};
+use frame_support::{dispatch::DispatchResult, ensure, RuntimeDebug};
+use serde::{Deserialize, Deserializer, Serialize};
+use sp_std::vec::Vec;
 
 pub use pallet::*;
 
@@ -15,11 +16,35 @@ mod tests;
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 
+
+#[derive(Debug, Serialize, Deserialize, Encode, Decode, Default)]
+pub struct PersonInfo {
+    #[serde(deserialize_with = "de_string_to_bytes")]
+    name: Vec<u8>,
+    //姓名
+    #[serde(deserialize_with = "de_string_to_bytes")]
+    id_card: Vec<u8>,
+    //身份证号码
+    height: u16,
+    //身高 mm
+    weight: u16,
+    //体重 g
+    #[serde(deserialize_with = "de_string_to_bytes")]
+    chronic: Vec<u8>, //慢性病
+}
+
+pub fn de_string_to_bytes<'de, D>(de: D) -> Result<Vec<u8>, D::Error>
+    where D: Deserializer<'de> {
+    let s: &str = Deserialize::deserialize(de)?;
+    Ok(s.as_bytes().to_vec())
+}
+
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+    use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
-    use sp_std::vec::Vec;
+
+    use super::*;
 
     /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
@@ -38,10 +63,8 @@ pub mod pallet {
     #[pallet::storage]
     #[pallet::getter(fn relation_id_cards)]
     /// 账户和身份证关联关系 RelationType u8
-    pub type RelationIdCards<T: Config> = StorageDoubleMap<_, Twox64Concat, T::AccountId, Twox64Concat, T::RelationType, Vec<u8>>;
+    pub type RelationIdCards<T: Config> = StorageDoubleMap<_, Twox64Concat, T::AccountId, Twox64Concat, T::RelationType, PersonInfo>;
 
-    // Pallets use events to inform users when important changes are made.
-    // https://substrate.dev/docs/en/knowledgebase/runtime/events
     #[pallet::event]
     #[pallet::metadata(T::AccountId = "AccountId")]
     #[pallet::generate_deposit(pub (super) fn deposit_event)]
@@ -58,6 +81,7 @@ pub mod pallet {
         NoneValue,
         /// Errors should have helpful documentation associated with them.
         StorageOverflow,
+        JsonParamError,
     }
 
     #[pallet::hooks]
@@ -65,11 +89,22 @@ pub mod pallet {
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        /// 绑定亲属身份证
+        /// 绑定亲属信息
         #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-        pub fn bind(origin: OriginFor<T>, relation_type: T::RelationType, id_card: Vec<u8>) -> DispatchResultWithPostInfo {
-            let sender = ensure_signed(origin)?;
-            RelationIdCards::<T>::insert(&sender, relation_type, id_card);
+        pub fn bind(origin: OriginFor<T>, relation_type: T::RelationType, json: Vec<u8>) -> DispatchResultWithPostInfo {
+            let _sender = ensure_signed(origin)?;
+            // let data1 = str::from_utf8(&json).map_err(|_| <Error<T>>::JsonParamError)?;
+            // let data1 = String::from_utf8(json).map_err(|_| <Error<T>>::JsonParamError)?;
+            let data = r#"
+            {
+                "name": "luffy",
+                "id_card": "43",
+                "height": 43,
+                "weight": 43,
+                "chronic": "123456"
+            }"#;
+            let ps_info: PersonInfo = serde_json::from_str(&data).map_err(|_| <Error<T>>::JsonParamError)?;
+            RelationIdCards::<T>::insert(&_sender, relation_type, ps_info);
             Ok(().into())
         }
     }
